f7ed47ae788431d6a903c520f76a5d5ef899bb54
Vladislav <vlydikmetal666@mail.ru>
2024-12-01 16:37:20 +0300

    fix bugs and add section adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import animation from './modules/animatiom'
 import burgerMenu from './modules/burger'
 
+
 document.addEventListener('DOMContentLoaded', () => {
   burgerMenu()
   animation()
--- src/js/modules/animatiom.js
+++ src/js/modules/animatiom.js
@@ -3,17 +3,125 @@ const animation = () => {
 
   gsap.to('.header__product-img', {
     rotation: 15,
-    duration: 2, 
-    repeat: -1, 
-    yoyo: true, 
-    ease: 'power2.inOut', 
+    duration: 2,
+    repeat: -1,
+    yoyo: true,
+    ease: 'power2.inOut',
   })
   gsap.to('.header__product-img--second', {
     rotation: -15,
-    duration: 2, 
-    repeat: -1, 
-    yoyo: true, 
-    ease: 'power2.inOut', 
+    duration: 2,
+    repeat: -1,
+    yoyo: true,
+    ease: 'power2.inOut',
+  })
+
+  gsap.to('.section__cards-img', {
+    rotation: 15,
+    duration: 2,
+    repeat: -1,
+    yoyo: true,
+    ease: 'power2.inOut',
+  })
+
+  gsap.to('.section__cards-img--second', {
+    rotation: -15,
+    duration: 2,
+    repeat: -1,
+    yoyo: true,
+    ease: 'power2.inOut',
+  })
+
+  const items = document.querySelectorAll('.header__product-item')
+
+  const animateItem = (item, index) => {
+    let initialY = window.innerWidth <= 644 ? 0 : -100;
+
+    gsap.fromTo(
+      initialY,
+      {
+        start: 'bottom bottom',
+        markers: true,
+        opacity: 0,
+        y: -100,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        delay: index * 0.15,
+        duration: 1,
+      },
+    )
+  
+    gsap.fromTo(
+      item,
+      {
+        opacity: 0,
+        y: -100,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        delay: index * 0.15,
+        duration: 1,
+      },
+    )
+  }
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry, index) => {
+        if (entry.isIntersecting) {
+          animateItem(entry.target, index)
+          observer.unobserve(entry.target)
+        }
+      })
+    },
+    {
+      threshold: 0.1,
+    },
+  )
+
+  items.forEach((item) => {
+    observer.observe(item)
+  })
+
+  gsap.fromTo('.header__body-info h1', {
+    opacity: 0,
+    x: -100,
+  }, {
+    opacity: 1,
+    x: 0,
+    duration: 1,
+  })
+
+  gsap.fromTo('.header__body-info p', {
+    opacity: 0,
+    x: -100,
+  }, {
+    opacity: 1,
+    x: 0,
+    duration: 1,
+  })
+
+  gsap.fromTo('.header__body-btns', {
+    opacity: 0,
+
+    x: 100,
+  }, {
+    opacity: 1,
+    x: 0,
+    duration: 1,
+  })
+
+  gsap.fromTo('.header__body-images', {
+    opacity: 0,
+    y: -100,
+    start: 'bottom bottom',
+  }, {
+    opacity: 1,
+    y: 0,
+    duration: 1,
   })
 }
 
